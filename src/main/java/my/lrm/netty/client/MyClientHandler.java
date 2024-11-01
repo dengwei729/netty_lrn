cba0a15600999b4c37d5f68e42898a211f41fdce
dw113073 <dw113073@alibaba-inc.com>
2024-11-01 16:03:31 +0800

    netty学习

---
+++ src/main/java/my/lrm/netty/client/MyClientHandler.java
@@ -0,0 +1,28 @@
+package my.lrm.netty.client;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author dw113073.邓伟
+ * @since 2024/11/1 15:39
+ * @date 2024/11/01
+ */
+public class MyClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        //发送消息到服务端
+        ctx.writeAndFlush(Unpooled.copiedBuffer("歪比巴卜~茉莉~Are you good~马来西亚~", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //接收服务端发送过来的消息
+        ByteBuf byteBuf = (ByteBuf) msg;
+        System.out.println("收到服务端" + ctx.channel().remoteAddress() + "的消息：" + byteBuf.toString(CharsetUtil.UTF_8));
+    }
+
+}
